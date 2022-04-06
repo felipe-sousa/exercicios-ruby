dc9f54b8958feea25f60682cbadc3c866bb38e09
Felipe de Sousa <felipedesousa2001@gmail.com>
2022-04-06 12:43:37 -0300

    add exe11
    Adicionar exercício 11.

---
+++ src/exe11.rb
@@ -0,0 +1,58 @@
+=begin
+Faça um programa que receba quatro valores: I, A, B e C.
+Desses valores, I é um Inteiro Positivo, A, B e C são Reais.
+Escreva os números A, B e C obedecendo a tabela a seguir.
+Suponha que o valor digitado para I seja sempre um valor válido, ou seja, 1, 2 ou 3
+e que os números reais sejam diferentes um do outro.
+Valor de I - Forma a Escrever
+    1      - A, B, C em ordem crescente
+    2	   - A, B, C em ordem decrescente
+    3	   - O maior fica entre os outros dois números
+=end
+
+puts "Digite o valor de I: "
+i = gets
+
+puts "Digite o valor de A: "
+a = gets
+
+puts "Digite o valor de B: "
+b = gets
+
+puts "Digite o valor de C: "
+c = gets
+
+i = i.to_i
+a = a.to_f
+b = b.to_f
+c = c.to_f
+
+maior = a
+if b > maior || c > maior
+    if b > c
+        maior = b
+    else
+        maior = c
+    end
+end
+
+menor = a
+if b < menor || c < menor
+    if b < c
+        menor = b
+    else
+        menor = c
+    end
+end
+
+meio = (a + b + c) - (maior + menor)
+
+if i == 1
+    puts menor.to_s + ", " + meio.to_s  + ", " + maior.to_s
+elsif i == 2 
+    puts maior.to_s + ", " + meio.to_s  + ", " + menor.to_s
+elsif i == 3
+    puts meio.to_s + ", " + maior.to_s  + ", " + menor.to_s
+else
+    puts "A opção selecionada é inválida."
+end
